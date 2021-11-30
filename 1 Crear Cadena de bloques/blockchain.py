a4347aef571aa5982ad24e3ab67155204d38abea
sergioarnold87 <anol1987@hotmail.com>
2021-11-29 21:27:51 -0300

    Create blockchain.py
    Programamos los metodos de create_blockm get_previous_block y proof_of_word

---
+++ 1 Crear Cadena de bloques/blockchain.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 20:27:37 2021
+
+@author: sergio
+"""
+
+# Modulo 1 - Crear una Cadena de Bloques
+
+# Para Instalar:
+# Flask==0.12.2: pip install flask==0.12.2
+# Cliente HTTP Postman
+
+# Importar las librerías
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Parte 1 - Crear la Cadena de Bloques
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        block = {'index' : len(self.chain)+1,
+                 'timestamp' : str(datetime.datetime.now()),
+                 'proof' : proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    def proof_of_word(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    
+        
+        
+        
